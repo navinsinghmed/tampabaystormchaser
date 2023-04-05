2a51d685b0367b9a966e3f4294960681e322736f
Navin N. Singh <30394693+navinsinghmed@users.noreply.github.com>
2023-04-04 23:37:30 -0400

    Add files via upload

---
+++ data_bases_ex.sql
@@ -0,0 +1,100 @@
+--from the terminal run:
+--psql < data_bases_ex.sql
+
+DROP DATABASE IF EXISTS data_bases_ex.sql;
+
+CREATE DATABASE data_bases_ex;
+
+\c data_bases_ex
+
+CREATE TABLE planets 
+{
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    orbital_period_in_years FLOAT NOT NULL,
+    orbits_around TEXT NOT NULL,
+    galaxy TEXT NOT NULL,
+    moons TEXT[]
+};
+
+INSERT INTO planets
+  (name, orbital_period_in_years, orbits_around, galaxy, moons)
+VALUES
+  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
+  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
+  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
+  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
+  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
+  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+  
+  -- from the terminal run:
+-- psql < music.sql
+
+DROP DATABASE IF EXISTS music;
+
+CREATE DATABASE music;
+
+\c music
+
+CREATE TABLE songs
+(
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  duration_in_seconds INTEGER NOT NULL,
+  release_date DATE NOT NULL,
+  artists TEXT[] NOT NULL,
+  album TEXT NOT NULL,
+  producers TEXT[] NOT NULL
+);
+
+INSERT INTO songs
+  (title, duration_in_seconds, release_date, artists, album, producers)
+VALUES
+  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
+  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
+  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
+  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
+  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
+  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
+  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
+  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
+  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
+  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+
+-- from the terminal run:
+-- psql < air_traffic.sql
+
+DROP DATABASE IF EXISTS air_traffic;
+
+CREATE DATABASE air_traffic;
+
+\c air_traffic
+
+CREATE TABLE tickets
+(
+  id SERIAL PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  seat TEXT NOT NULL,
+  departure TIMESTAMP NOT NULL,
+  arrival TIMESTAMP NOT NULL,
+  airline TEXT NOT NULL,
+  from_city TEXT NOT NULL,
+  from_country TEXT NOT NULL,
+  to_city TEXT NOT NULL,
+  to_country TEXT NOT NULL
+);
+
+INSERT INTO tickets
+  (first_name, last_name, seat, departure, arrival, airline, from_city, from_country, to_city, to_country)
+VALUES
+  ('Jennifer', 'Finch', '33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 'United', 'Washington DC', 'United States', 'Seattle', 'United States'),
+  ('Thadeus', 'Gathercoal', '8A', '2018-12-19 12:45:00', '2018-12-19 16:15:00', 'British Airways', 'Tokyo', 'Japan', 'London', 'United Kingdom'),
+  ('Sonja', 'Pauley', '12F', '2018-01-02 07:00:00', '2018-01-02 08:03:00', 'Delta', 'Los Angeles', 'United States', 'Las Vegas', 'United States'),
+  ('Jennifer', 'Finch', '20A', '2018-04-15 16:50:00', '2018-04-15 21:00:00', 'Delta', 'Seattle', 'United States', 'Mexico City', 'Mexico'),
+  ('Waneta', 'Skeleton', '23D', '2018-08-01 18:30:00', '2018-08-01 21:50:00', 'TUI Fly Belgium', 'Paris', 'France', 'Casablanca', 'Morocco'),
+  ('Thadeus', 'Gathercoal', '18C', '2018-10-31 01:15:00', '2018-10-31 12:55:00', 'Air China', 'Dubai', 'UAE', 'Beijing', 'China'),
+  ('Berkie', 'Wycliff', '9E', '2019-02-06 06:00:00', '2019-02-06 07:47:00', 'United', 'New York', 'United States', 'Charlotte', 'United States'),
+  ('Alvin', 'Leathes', '1A', '2018-12-22 14:42:00', '2018-12-22 15:56:00', 'American Airlines', 'Cedar Rapids', 'United States', 'Chicago', 'United States'),
+  ('Berkie', 'Wycliff', '32B', '2019-02-06 16:28:00', '2019-02-06 19:18:00', 'American Airlines', 'Charlotte', 'United States', 'New Orleans', 'United States'),
+  ('Cory', 'Squibbes', '10D', '2019-01-20 19:30:00', '2019-01-20 22:45:00', 'Avianca Brasil', 'Sao Paolo', 'Brazil', 'Santiago', 'Chile');
