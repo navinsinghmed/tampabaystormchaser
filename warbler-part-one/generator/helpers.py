7686ac8972bc9a5c4fcbb56fb85865e3f782df0b
Navin N. Singh <30394693+navinsinghmed@users.noreply.github.com>
2023-04-19 18:11:34 -0400

    Add files via upload

---
+++ warbler-part-one/generator/helpers.py
@@ -0,0 +1,13 @@
+"""Support functions for CSV generation."""
+
+from datetime import datetime
+from random import uniform
+
+def get_random_datetime(year_gap=2):
+    """Get a random dateline within the last few years."""
+    
+    now = datetime.now()
+    then = now.replace(year=now.year - year_gap)
+    random_timestamp = uniform(then.timestamp(), now.timestamp())
+    
+    return datetime.fromtimestamp(random_timestamp)
